77b43861d9c81109bfdbe6c2fd1052b1319eee8d
Marcelo Tambalo <marcelo.zokis@gmail.com>
2013-05-30 17:24:14 -0300

    adicionando template tag de Inteiro e Menetaria

--- localbr/templatetags/numberformat_br.py
+++ localbr/templatetags/numberformat_br.py
@@ -5,5 +5,17 @@ register = template.Library()
 
 
 @register.filter
-def numberformat_br(number):
+def numberformat_br(number, decimal_pos=2):
+    return format(number, decimal_sep=',', decimal_pos=decimal_pos, grouping=3, thousand_sep='.', force_grouping=3)
+
+
+@register.filter
+def number_br_int(number):
+    number = int(number)
+    return format(number, decimal_sep='', decimal_pos=0, grouping=3, thousand_sep='.', force_grouping=3)
+
+
+@register.filter
+def monetary_br(number):
+    number = float(number)
     return format(number, decimal_sep=',', decimal_pos=2, grouping=3, thousand_sep='.', force_grouping=3)
