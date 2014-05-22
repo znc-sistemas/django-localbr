59a02361992a39e571c7b4bd30d986b01aecccf7
Luiz Vital <luiz.vital@gmail.com>
2014-05-22 11:19:32 -0300

    Trata entrada de string vazia e None em templatetag filter

--- localbr/templatetags/numberformat_br.py
+++ localbr/templatetags/numberformat_br.py
@@ -6,16 +6,22 @@ register = template.Library()
 
 @register.filter
 def numberformat_br(number, decimal_pos=2):
+    if number in (None, ''):
+        return ''
     return format(number, decimal_sep=',', decimal_pos=decimal_pos, grouping=3, thousand_sep='.', force_grouping=3)
 
 
 @register.filter
 def number_br_int(number):
+    if number in (None, ''):
+        return ''
     number = int(number)
     return format(number, decimal_sep='', decimal_pos=0, grouping=3, thousand_sep='.', force_grouping=3)
 
 
 @register.filter
 def monetary_br(number):
+    if number in (None, ''):
+        return ''
     number = float(number)
     return format(number, decimal_sep=',', decimal_pos=2, grouping=3, thousand_sep='.', force_grouping=3)
