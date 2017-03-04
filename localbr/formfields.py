433a00e0da66963a7fe742a1537ad92b6458d206
Fabiano Almeida <fabiano@znc.com.br>
2017-03-04 16:00:15 -0300

    ref path para importar do 'widget' e inexistencia de 'smart_unicode' no django 1.9

--- localbr/formfields.py
+++ localbr/formfields.py
@@ -3,8 +3,17 @@
 from datetime import date
 
 from django.forms import FloatField, DecimalField, MultiValueField, ValidationError
-from django.utils.encoding import smart_unicode
-from widgets import BRDecimalWidget, PointWidget, BRFloatWidget, BRJsDateWidget
+
+try:
+    from django.utils.encoding import smart_char as smart_char
+except:
+    from django.utils.encoding import smart_str  as smart_char
+
+try:
+    from widgets import BRDecimalWidget, PointWidget, BRFloatWidget
+except:
+    from localbr.widgets import BRDecimalWidget, PointWidget, BRFloatWidget
+
 from django.forms.fields import EMPTY_VALUES
 from django.utils.safestring import mark_safe
 from django.forms.fields import Field
@@ -42,7 +51,7 @@ class BRPhoneNumberField(Field):
         super(BRPhoneNumberField, self).clean(value)
         if value in EMPTY_VALUES:
             return u''
-        value = re.sub('(\(|\)|\s+)', '', smart_unicode(value))
+        value = re.sub('(\(|\)|\s+)', '', smart_char(value))
         m = re.compile(
             r'^(\d{2})[-\.]?((\d{5})|(\d{4}))[-\.]?(\d{4})$').search(value)
         if m:
